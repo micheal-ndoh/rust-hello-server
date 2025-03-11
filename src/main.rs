24102c89f017dee0debcc58890c2deb2358eb4ee
micheal-ndoh <michaelndoh9@gmail.com>
2025-03-11 10:07:53 +0100

    remove scriber init for tracking user requests

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
-
-
 use axum::{
-    routing::{get, post}, Router,
+    routing::get,
+    Router,
 };
-
 
 #[tokio::main]
 async fn main() {
-    
-    tracing_subscriber::fmt::init();
-
     let app = Router::new()
-       
         .route("/", get(root))
         .route("/hello", get(hello));
- 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
